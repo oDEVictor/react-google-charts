c6aa86ad1d3765bb24fd90c3d043bcfa0d773d75
Victoor <vvictorcs18@gmail.com>
2023-03-13 16:14:28 -0300

    Atualizando...

---
+++ Desktop/newbranchcharts/dashboard-frontend/src/TableBasic/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const TableStyled = styled.div`
+
+table{
+    background: linear-gradient(to bottom, #FFFFFF, #E6F3FF, #B3E0FF);;
+}
+
+.select-table{
+    margin-top: 40px;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 10px;
+    width: 80%;
+}
+
+`
+;
